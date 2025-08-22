785e2759fd1dabcf3e2b38f3b16520bf88c50900
JorgeGoytia37 <31255378+goytiaj6@users.noreply.github.com>
2025-08-22 01:58:28 -0600

    Update K-NN.py
    Se agregaron las lecturas de los archivos .xlsx

--- K-NN.py
+++ K-NN.py
@@ -3,9 +3,20 @@
 ### Importamos las bibliotecas necesarias
 import pandas as pd
 
+# Leemos los datos del dataset base
+df = pd.read_excel("animals.xlsx", engine="openpyxl") # type: ignore
 
+# Mostrar las primeras y útimas filas
+# print(df)
 
+atributos = df.iloc[:, :-1].values.tolist()
+clases = df.iloc[:, -1].tolist()
 
+# Leemos los datos a clasificar
+df_nuevo = pd.read_excel("animals_clasificar.xlsx") # type: ignore
+atributos_nuevo = df_nuevo.values.tolist()
 
+# Mostrar las primeras y útimas filas
+# print(df_nuevo)
 
 ### FIN DEL PROGRAMA ###
